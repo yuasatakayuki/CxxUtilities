b55242502be4a6694161ca6a39af487e0522ba78
Takayuki Yuasa <yuasatakayuki@gmail.com>
2011-07-24 18:06:54 -0700

    A common header file which reduces time to open header files referred to from invididual header files.

---
+++ includes/CxxUtilities/CommonHeader.hh
@@ -0,0 +1,38 @@
+/*
+ * CommonHeader.hh
+ *
+ *  Created on: Jun 17, 2011
+ *      Author: yuasa
+ */
+
+#ifndef COMMONHEADER_HH_
+#define COMMONHEADER_HH_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <bitset>
+#include <fstream>
+#include <map>
+#include <queue>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+#endif /* COMMONHEADER_HH_ */
