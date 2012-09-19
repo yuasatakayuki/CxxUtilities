f2bf9b35a5d9aa7b2c905608e7f364283a3048e9
Takayuki Yuasa <yuasatakayuki@gmail.com>
2012-09-19 15:04:13 +0900

    a method to get time information was added

--- includes/CxxUtilities/Time.hh
+++ includes/CxxUtilities/Time.hh
@@ -11,11 +11,23 @@
 #include "CxxUtilities/CommonHeader.hh"
 
 namespace CxxUtilities {
+class TimeValues {
+public:
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hour;
+	uint8_t min;
+	uint8_t sec;
+	uint8_t msec;
+	uint16_t yday;
+};
+
 class Time {
 public:
 	/** Returns the current date/time formatted accordingly.
 	 */
-	static std::string getCurrentTimeAsString(std::string format ="%Y-%m-%d %I:%M:%S") {
+	static std::string getCurrentTimeAsString(std::string format ="%Y-%m-%d %H:%M:%S") {
 		time_t timer = time(NULL);
 		struct tm *date;
 		char str[1024];
@@ -27,11 +39,27 @@ public:
 	/** Returns the current date/time formatted accordingly.
 	 */
 	static std::string getCurrentTimeYYYYMMDD_HHMM() {
-		return getCurrentTimeAsString("%Y%m%d_%I%M");
+		return getCurrentTimeAsString("%Y%m%d_%H%M");
 	}
 
 	static time_t getUNIXTime(){
 		return time(0);
+	}
+
+	static TimeValues getCurrentTimeAsNumbers(){
+		time_t timer = time(NULL);
+		struct tm *date;
+		date = localtime(&timer);
+		TimeValues timeValues;
+		timeValues.year=date->tm_year+1900;
+		timeValues.month=date->tm_mon;
+		timeValues.day=date->tm_mday;
+		timeValues.hour=date->tm_hour;
+		timeValues.min=date->tm_min;
+		timeValues.sec=date->tm_sec;
+		timeValues.msec=0;
+		timeValues.yday=date->tm_yday;
+		return timeValues;
 	}
 };
 }
