0e05140e3f4f5d3f2ec01e5291baf172757c2458
Takayuki Yuasa <yuasatakayuki@gmail.com>
2014-04-06 15:42:55 +0900

    Counter.hh - toString() was added

--- includes/CxxUtilities/Counter.hh
+++ includes/CxxUtilities/Counter.hh
@@ -20,7 +20,7 @@ namespace CxxUtilities {
 class Counter {
 private:
 	std::string id;
-	uint64_t counter;
+	size_t counter;
 
 public:
 	Counter(std::string id){
@@ -29,42 +29,58 @@ public:
 	}
 
 public:
-	inline void increment(uint64_t byN=1){
-		counter=counter+byN;
+	inline void increment(size_t byN = 1) {
+		counter = counter + byN;
 	}
 
-	inline void decreent(uint64_t byN=1){
-		counter=counter-byN;
+	inline void decreent(size_t byN = 1) {
+		counter = counter - byN;
 	}
 
-	inline uint64_t getValue(){
+	inline size_t getValue() {
 		return counter;
 	}
 
-	inline uint64_t getCounterValue(){
+	inline size_t getCounterValue() {
 		return counter;
 	}
 
-	inline void clear(){
-		counter=0;
+	inline void clear() {
+		counter = 0;
 	}
 
-	inline void setValue(uint64_t counter){
-		this->counter=counter;
+	inline void setValue(size_t counter) {
+		this->counter = counter;
 	}
 
-	inline void setCounterValue(uint64_t counter){
-		this->counter=counter;
+	inline void setCounterValue(size_t counter) {
+		this->counter = counter;
 	}
 
 public:
 	std::string getID(){
 		return id;
 	}
+
+public:
+	std::string toString(size_t widthOfIDLabel = 10) {
+		std::stringstream ss;
+		ss << std::setw(widthOfIDLabel) << id << " : " << std::dec << counter;
+		return ss.str();
+	}
+
+public:
+	size_t getIDLength() {
+		return id.size();
+	}
 };
 
+/** A class that collects counter instances.
+ * Note: Typenamed version of this called "Counters" is available instead of "Counters_".
+ * This class definition with template is for defining class variable in the header file.
+ */
 template<typename T>
-class Counters_ : public Counter {
+class Counters_ {
 private:
 	static std::map<std::string,Counter*> counters;
 
@@ -118,6 +134,40 @@ public:
 			delete it->second;
 		}
 		counters.clear();
+	}
+
+public:
+	/** Increments counter value. If the specified counter does not exist,
+	 * a new counter instance will be created and then incremented.
+	 * @param[in] id counter id
+	 * @param[in] byN increment value (default = 1)
+	 */
+	static void increment(std::string id, size_t byN = 1) {
+		getCounterInstance(id)->increment(byN);
+	}
+
+public:
+	std::string toString(){
+		using namespace std;
+		size_t width=getMaximumIDLabelWidth();
+		std::stringstream ss;
+		std::map<std::string, Counter*>::iterator it = counters.begin();
+		for (; it != counters.end(); it++) {
+			ss << it->second->toString(width) << endl;
+		}
+		return ss.str();
+	}
+
+public:
+	size_t getMaximumIDLabelWidth() {
+		size_t max = 0;
+		std::map<std::string, Counter*>::iterator it = counters.begin();
+		for (; it != counters.end(); it++) {
+			if (max < it->first.size()) {
+				max  = it->first.size();
+			}
+		}
+		return max;
 	}
 };
 
